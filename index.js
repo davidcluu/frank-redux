c100c11f37f872a4aa5e205d39e7118f13f56930
David Luu <luu.david.c@gmail.com>
2016-10-26 22:18:45 -0700

    Test Release

---
+++ index.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV === 'production') {
+  process.env.webpackAssets = JSON.stringify(require('./dist/manifest.json'));
+  process.env.webpackChunkAssets = JSON.stringify(require('./dist/chunk-manifest.json'));
+
+  require('./dist/server.bundle.js');
+} else if (process.env.NODE_ENV === 'development') {
+  require('babel-register')({
+    plugins: [
+      [
+        'babel-plugin-webpack-loaders',
+        {
+          config: './webpack.config.babel.js',
+          verbose: false
+        }
+      ]
+    ]
+  });
+
+  require('babel-polyfill');
+
+  require('./server/server');
+}
